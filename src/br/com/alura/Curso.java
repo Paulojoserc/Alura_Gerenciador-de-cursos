66c6278235262b1edf143917dacee2638587201c
Paulojoserc <pjrc83@gmail.com>
2023-03-20 23:46:32 -0300

    Map

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -1,9 +1,13 @@
 package br.com.alura;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class Curso {
@@ -12,6 +16,7 @@ public class Curso {
 	private String instrutor;
 	private List<Aula> aulas = new ArrayList<>();
 	private Set<Aluno> alunos = new HashSet<>();
+	private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
 	
 
 	public Curso(String nome, String instrutor) {
@@ -54,6 +59,7 @@ public class Curso {
 
 	public void matricula(Aluno aluno) {
 		this.alunos.add(aluno);
+		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
 	}
 	
 	public Set<Aluno> getAlunos() {
@@ -63,6 +69,12 @@ public class Curso {
 	public boolean estaMatriculado(Aluno aluno) {
 		
 		return this.alunos.contains(aluno);
+	}
+
+	public Aluno buscaMatriculado(int numero)  {
+		if(!matriculaParaAluno.containsKey(numero))
+			throw new NoSuchElementException();
+		return matriculaParaAluno.get(numero);
 	}
 
 }
---
+++ src/br/com/alura/TesteMap.java
@@ -0,0 +1,41 @@
+package br.com.alura;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class TesteMap {
+	public static void main(String[] args) {
+		Map<String, Integer> nomesParaIdade = new HashMap<>();
+		nomesParaIdade.put("Paulo", 31);
+		nomesParaIdade.put("Adriano", 25);
+		nomesParaIdade.put("Alberto", 33);
+		nomesParaIdade.put("Guilherme", 26);
+
+		// Para acessar as chaves devemos executar:
+		Set<String> chaves = nomesParaIdade.keySet();
+		for (String nome : chaves) {
+			System.out.println(nome);
+		}
+
+		// E para pegar os valores usamos:
+		Collection<Integer> valores = nomesParaIdade.values();
+		for (Integer idade : valores) {
+			System.out.println(idade);
+		}
+
+		//    Agora só falta a terceira coleção que devolve a associação. 
+		//    Para tal, existe o método entrySet() e cada associação é representado através da classe Entry:
+
+		//Set<Entry<String, Integer>> associacoes = nomesParaIdade.entrySet();
+		
+		// Repare que o método devolve um Set de Entry (respire fundo!!). Para acessar essa coleção basta usar o foreach:
+		
+		Set<Entry<String, Integer>> associacoes = nomesParaIdade.entrySet();    
+		for (Entry<String, Integer> associacao : associacoes) {
+		    System.out.println(associacao.getKey() + " - " + associacao.getValue());
+		}
+	}
+}
